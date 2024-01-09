4057a5f03c800147bd21c591e4e70faa96495242
Jonathan Ribeiro <156020875+JonthanSilva@users.noreply.github.com>
2024-01-09 17:17:39 -0300

    Delete ReadOnlyInteger.java

--- ReadOnlyInteger.java
+++
@@ -1,9 +0,0 @@
-/**
- * ReadOnlyInteger
- */
-public class ReadOnlyInteger {
-        public static void  main(String[] args) {
-            final int Var = 150;
-            System.out.println(Var);
-          }
-    }
